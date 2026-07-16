a454b82c2de3e47bfbb40a1900ee54f9bf6552fa
Ron Gilmour <rgilmour70@gmail.com>
2026-07-16 11:05:24 -0400

    make OtherSearchComponent update when user does a new search

--- src/app/other-search/other-search.component.ts
+++ src/app/other-search/other-search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'custom-other-search',
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './other-search.component.html',
   styleUrl: './other-search.component.scss',
 })
-export class OtherSearchComponent implements OnInit {
+export class OtherSearchComponent implements OnInit, OnDestroy {
   googleBaseUrl: string =
     'https://scholar.google.com/scholar?hl=en&as_sdt=0%2C33&inst=7210957415625843320';
   googleSearchUrl = '';
@@ -17,11 +17,29 @@ export class OtherSearchComponent implements OnInit {
   ebscoSuffix: string =
     '&defaultdb=8gh%2Cahl%2Cair%2Caph%2Capn%2Casu%2Cbuh%2Cbwh%2Cbxh%2Ccin20%2Ccmedm%2Ccms%2Ce870sww%2Ceric%2Chch%2Chev%2Cibh%2Ckah%2Clfh%2Clxh%2Cmft%2Cmzh%2Cnfh%2Cnlebk%2Cpdh%2Cphl%2Cpsyh%2Cqth%2Creh%2Crft%2Crgr%2Cs3h%2Cser%2Csih%2Ctfh%2Ctrh%2C&auth-callid=0ad92c39-e993-42ad-9c3e-22c0d0466f14&skipResultsFetch=true&sqId=sq%3A273857da-97bf-4018-902e-1c53432a5a37';
 
+  private lastQuery = '';
+  private pollHandle: ReturnType<typeof setInterval> | undefined;
+
   ngOnInit(): void {
+    this.checkQuery();
+    // Poll for URL changes since we can't rely on ActivatedRoute here
+    this.pollHandle = setInterval(() => this.checkQuery(), 500);
+  }
+
+  ngOnDestroy(): void {
+    if (this.pollHandle) {
+      clearInterval(this.pollHandle);
+    }
+  }
+
+  private checkQuery(): void {
     const params = new URLSearchParams(window.location.search);
     const rawQuery = params.get('query') ?? '';
-    this.convertToGoogle(rawQuery);
-    this.convertToEbsco(rawQuery);
+    if (rawQuery !== this.lastQuery) {
+      this.lastQuery = rawQuery;
+      this.convertToGoogle(rawQuery);
+      this.convertToEbsco(rawQuery);
+    }
   }
 
   private convertToGoogle(primoSearch: string): void {
