a14cf2df33f871e22f060a54643df9b63b4b3d7b
Ron Gilmour <rgilmour70@gmail.com>
2026-08-03 11:19:48 -0400

    tweaks to homepage in case we use it

--- src/app/other-search/other-search.component.ts
+++ src/app/other-search/other-search.component.ts
@@ -17,7 +17,10 @@ export class OtherSearchComponent implements OnInit, OnDestroy {
   ebscoSuffix: string =
     '&defaultdb=8gh%2Cahl%2Cair%2Caph%2Capn%2Casu%2Cbuh%2Cbwh%2Cbxh%2Ccin20%2Ccmedm%2Ccms%2Ce870sww%2Ceric%2Chch%2Chev%2Cibh%2Ckah%2Clfh%2Clxh%2Cmft%2Cmzh%2Cnfh%2Cnlebk%2Cpdh%2Cphl%2Cpsyh%2Cqth%2Creh%2Crft%2Crgr%2Cs3h%2Cser%2Csih%2Ctfh%2Ctrh%2C&auth-callid=0ad92c39-e993-42ad-9c3e-22c0d0466f14&skipResultsFetch=true&sqId=sq%3A273857da-97bf-4018-902e-1c53432a5a37';
 
+  isHomepage = false;
+
   private lastQuery = '';
+  private lastPathname = '';
   private pollHandle: ReturnType<typeof setInterval> | undefined;
 
   ngOnInit(): void {
@@ -33,6 +36,11 @@ export class OtherSearchComponent implements OnInit, OnDestroy {
   }
 
   private checkQuery(): void {
+    const pathname = window.location.pathname;
+    if (pathname !== this.lastPathname) {
+      this.lastPathname = pathname;
+      this.isHomepage = pathname.includes('home');
+    }
     const params = new URLSearchParams(window.location.search);
     const rawQuery = params.get('query') ?? '';
     if (rawQuery !== this.lastQuery) {
